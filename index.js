c6a45e3eeef2ad8cb4a080fa24c9f38412a8faf0
GamingCoder <coreoder@gmail.com>
2014-03-23 19:54:19 +0100

    Install underscore and write not working version of index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+var Q = require('q');
+var _ = require('underscore');
+
 function defOpts () {}
 
-// 
+defOpts.prototype.opts = function(opts, defaults, cb) {
+	var deffered = Q.defer();
+
+
+
+	return deffered.promise.nodeify(cb);
+};
+
 
 //  export defOpts class
 exports = module.exports = new defOpts();
